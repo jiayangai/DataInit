f9886e8a866af137711a406177d08b57d47fc308
136802833@qq.com <jy136802833@qq.com>
2016-02-16 14:13:52 +0800

    测试

---
+++ src/com/cnp/Init/TestGit.java
@@ -0,0 +1,23 @@
+/**
+  * @FileName: TestGit.java
+  * @Author 
+  * @Description:
+  * @Date 2016年2月16日 下午2:13:16
+  * @CopyRight CNP Corporation
+  */
+package com.cnp.Init;
+
+public class TestGit{
+
+    /**   
+     * @Title: main   
+     * @Description:TODO  
+     * @param args void
+     */
+
+    public static void main(String[] args){
+        // TODO Auto-generated method stub
+
+    }
+
+}
